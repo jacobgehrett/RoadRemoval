1b487ee1cc117a0ba8cf20440a00f489bebc132f
jgehrett <70722825+jacobgehrett@users.noreply.github.com>
2021-06-14 17:17:18 -0600

    Need to try different approach

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import math
-
 import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image
 from scipy import signal, ndimage
+
+THRESHOLD = 50
 
 im = Image.open('Map_0009.tif')
 im_array = np.array(im)
 im_array = im_array[:, :, 0:3]
 
 
-# Used many functions from Lab 3!
+# Used many functions from Labs 3 and 2!
 
 
 def to_gray_scale(image):
@@ -21,17 +21,6 @@ def to_gray_scale(image):
 
 # Convert image to greyscale for easier edge detection
 gray_im = to_gray_scale(im_array)
-
-
-def convolution(image, kernel):
-    # Create a result buffer so that we don't affect the original image
-    result = np.zeros(image.shape)
-    for i in range(image.shape[1] - 2):
-        for j in range(image.shape[0] - 2):
-            result[j + 1, i + 1] = np.sum(np.multiply(
-                image[j + 1 - math.floor(kernel.shape[0] / 2):j + 1 + math.ceil(kernel.shape[0] / 2),
-                      i + 1 - math.floor(kernel.shape[1] / 2):i + 1 + math.ceil(kernel.shape[1] / 2)], kernel))
-    return result
 
 
 def edge_detect(image):
@@ -50,19 +39,40 @@ def edge_detect(image):
 # Perform edge detection on image
 edge_im = edge_detect(gray_im)
 
-fig = plt.figure(figsize=(10, 7))
+fig = plt.figure(figsize=(10, 10))
 
-rows = 2
+rows = 3
 columns = 2
 
-fig.add_subplot(rows, columns, 1)
+fig.add_subplot(rows, columns, 1, title="Raw Image")
+plt.imshow(im_array, cmap="Greys_r", vmin=0, vmax=255)
 
-median_im = ndimage.median_filter(edge_im, size=3)
+fig.add_subplot(rows, columns, 2, title="Gray Image")
+plt.imshow(gray_im, cmap="Greys_r", vmin=0, vmax=255)
 
+fig.add_subplot(rows, columns, 3, title="Edge Detection")
 plt.imshow(edge_im, cmap="Greys_r", vmin=0, vmax=255)
 
-fig.add_subplot(rows, columns, 2)
-
+# Perform median filtering on image
+median_im = ndimage.median_filter(edge_im, size=8)
+fig.add_subplot(rows, columns, 4, title="Median Filtering")
 plt.imshow(median_im, cmap="Greys_r", vmin=0, vmax=255)
 
+
+def threshold(image):
+    image = np.where(image > THRESHOLD, 255, 0)
+    return image
+
+
+# Perform thresholding on image
+false_im = threshold(median_im)
+fig.add_subplot(rows, columns, 5, title="False Coloring")
+plt.imshow(false_im, cmap="Greys_r", vmin=0, vmax=255)
+
 plt.show()
+
+
+# At this point, I am realizing that it is very difficult to identify the man-made features from the naturally-formed
+# ones, so I am trying a different approach. The script, "mainTwo.py" is my second attempt, in which I am using the
+# MatPlotLib GUI to manually select points on the image to blend through. If I can get that working, I will come back to
+# this.
